f6cdff87cb9df32fbeb0c4d704c77f5364d94694
Alex Denisova <alexadenisova@gmail.com>
2024-06-04 21:03:02 +0300

    removed unnecessary functions

--- course-work/sensordata.h
+++ course-work/sensordata.h
@@ -15,10 +15,5 @@ typedef struct
 } SensorData;
 
 uint64_t from_csv(SensorData arr[], uint64_t max_arr_size, FILE *input, uint64_t max_line_size, uint64_t *number_of_err);
-void append_data(SensorData arr[], uint64_t *arr_len, SensorData data);
-SensorData pop_data(SensorData arr[], uint64_t *arr_len, uint64_t idx);
-void sort_arr(SensorData arr[], uint64_t arr_len);
-void print_sensor_data(SensorData data);
-void print_sensor_data_arr(SensorData arr[], uint64_t arr_len);
 
 #endif
